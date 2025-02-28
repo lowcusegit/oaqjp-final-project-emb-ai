6c3f699bb68184a24a6105cc309980aca3a69ccc
lowcusegit <lowingchonghk@yahoo.com.hk>
2025-02-27 22:54:19 -0500

    task8

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""import packages"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,7 +6,8 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
-     # Retrieve the text to analyze from the request arguments
+    """Define the function sent_detector"""
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
@@ -14,12 +16,14 @@ def sent_detector():
         return "Invalid text! Please try again!"
     output = f"For the given statement, the system response is"\
         f" 'anger': {response['anger']}, 'disgust': {response['disgust']},"\
-        f" 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}."\
+        f" 'fear': {response['fear']}, 'joy': {response['joy']} and"\
+        f" 'sadness': {response['sadness']}."\
         f" The dominant emotion is {response['dominant_emotion']}"
     return output
 
 @app.route("/")
 def render_index_page():
+    """# Render the HTML template using render_index_page"""
     return render_template('index.html')
 
 if __name__ == "__main__":
